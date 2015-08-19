a306470178b23bd45dcbc20e3038c168c75c7977
Brian Lauber <constructible.truth@gmail.com>
2015-08-19 13:42:20 -0400

    Renamed /aqa to /aqa_project

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aqa.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aqa_project.settings")
 
     from django.core.management import execute_from_command_line
 
